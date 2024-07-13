c44bdaceb4454e826c16033fe299092907df88e5
Hrqlv <henriquelopesvelozo@gmail.com>
2024-07-13 15:06:46 -0300

    Ajustes nas API's

--- tests/pages/apiDogs.page.ts
+++ tests/pages/apiDogs.page.ts
@@ -14,4 +14,32 @@ export class ServicesAPI {
         expect(response.status(), `Request (breeds) failed\nStatus: ${response.status()} ${response.statusText()}`).toBe(200);
         return response;
     }
+
+    async getBreedById(id: string) {
+        const context = await request.newContext();
+        const response = await context.get(`${this.url}/breeds/${id}`);
+        expect(response.status(), `Request (breed by id) failed\nStatus: ${response.status()} ${response.statusText()}`).toBe(200);
+        return response;
+    }
+
+    async getFacts() {
+        const context = await request.newContext();
+        const response = await context.get(`${this.url}/facts`);
+        expect(response.status(), `Request (facts) failed\nStatus: ${response.status()} ${response.statusText()}`).toBe(200);
+        return response;
+    }
+
+    async getGroups() {
+        const context = await request.newContext();
+        const response = await context.get(`${this.url}/groups`);
+        expect(response.status(), `Request (groups) failed\nStatus: ${response.status()} ${response.statusText()}`).toBe(200);
+        return response;
+    }
+
+    async getGroupById(id: string) {
+        const context = await request.newContext();
+        const response = await context.get(`${this.url}/groups/${id}`);
+        expect(response.status(), `Request (group by id) failed\nStatus: ${response.status()} ${response.statusText()}`).toBe(200);
+        return response;
+    }
 }
